712bc42a53f62318622ce5fb5a7249859e8ceff3
asolove <asolove@gmail.com>
2016-07-26 12:34:47 -0600

    Check case with desuraging to Object.assign: also has problem.

--- index.js
+++ index.js
@@ -4,10 +4,18 @@ type Thingy = {
   a: bool,
 }
 
+// Plain object with undefined property type.
+// This correctly raises a type error.
 const t1: Thingy = {
-  a: true,
+  a: undefined
 }
 
+// Spread where inner object has undefined property type.
+// This incorrectly fails to raise a type error.
 const t2: Thingy = {
   ...{ a: undefined }
 }
+
+// Object.assign return value with undefined property type.
+// Also incorrectly fails to raise a type error.
+const t3: Thingy = Object.assign({}, { a: undefined })
